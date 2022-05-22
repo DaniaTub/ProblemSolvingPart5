a3994d77c3d095287c58e21684e8a63be5629572
Dania <daniatub8@gmail.com>
2022-05-22 16:09:47 +0300

    return the middle of the linked list

---
+++ src/com/jhf/leetcode/SolutionFive.java
@@ -0,0 +1,26 @@
+package com.jhf.leetcode;
+//return the middle of the linked list
+// Definition for singly-linked list.
+ class ListNode {
+      int val;
+     ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
+class SolutionFive {
+    public ListNode middleNode(ListNode head)
+    {
+        ListNode slow  = head;
+        ListNode faster = head;
+
+        while(faster != null && faster.next != null){
+            slow = slow.next;
+            faster = faster.next.next;
+        }
+
+        return slow;
+
+    }
+}
